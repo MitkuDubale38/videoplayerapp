de21d391851f6d829446881ac91d9bc1c86e11f5
MitkuDubale38 <mitkudubale3864@gmail.com>
2023-11-13 12:19:30 -0500

    ad progresss

--- lib/app/modules/player/controllers/player_controller.dart
+++ lib/app/modules/player/controllers/player_controller.dart
@@ -1,16 +1,23 @@
 import 'package:chewie/chewie.dart';
 import 'package:get/get.dart';
 import 'package:video_player/video_player.dart';
+import 'package:videoplayerapp/app/utils/helper/ad_manager.dart';
 
 class PlayerController extends GetxController {
   late VideoPlayerController _controller;
   late ChewieController chewieController;
   String videoUrl = "";
   VideoPlayerController get controller => _controller;
+  InterstitialAdManager _interstitialAdManager = InterstitialAdManager();
 
   @override
   void onInit() {
     super.onInit();
+    //
+    _interstitialAdManager.loadInterstitialAd();
+    // Load the interstitial ad when the video player starts
+    _interstitialAdManager.showInterstitialAd();
+    
     _initializeVideoPlayer().then((_) {
       update();
     });
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:sizer/sizer.dart';
 import 'package:get/get.dart';
@@ -9,7 +10,7 @@ import 'app/routes/app_pages.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // MobileAds.instance.initialize();
+  MobileAds.instance.initialize();
   final internetConnectivity = InternetConnectivity();
   await internetConnectivity.initialize();
   await Hive.initFlutter();
